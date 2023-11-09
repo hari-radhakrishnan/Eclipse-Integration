9d0f8367664862e02dff99c4889f44a5974c338a
hari-radhakrishnan <73172266+hari-radhakrishnan@users.noreply.github.com>
2023-11-09 18:23:59 +0530

    Second Commit

--- src/testProject/TestProject.java
+++ src/testProject/TestProject.java
@@ -13,10 +13,11 @@ public class TestProject {
 		driver.findElement(By.xpath("/html/body/app-root/body/header/div[2]/div/div/div[5]/div/div/div/div/a")).click();
 		Thread.sleep(3000);
 		driver.findElement(By.name("email")).sendKeys("testfee@yopmail.com");
-		Thread.sleep(3000);
+		Thread.sleep(4000);
 		driver.findElement(By.name("password")).sendKeys("Test@123");
 		driver.findElement(By.xpath("//*[@id=\"myModal\"]/div/div/form/div[2]/div[3]/div/input")).click();	
 
+		
 	}
 
 }
